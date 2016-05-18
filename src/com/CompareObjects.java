c48862e3791969866de8f08662e25addba7601c4
Vipul <vipul.srivastava@siemens.com>
2016-05-18 10:08:53 +0530

    initial checkin

---
+++ src/com/CompareObjects.java
@@ -0,0 +1,24 @@
+package com;
+
+public class CompareObjects 
+{
+
+	
+	
+	public static boolean equals(Object o1,Object o2)
+	{
+		boolean toReturn=false;
+		if(o1!=null && o2 !=null)
+		{
+			toReturn=o1.equals(o2);
+		}
+		else if(o1==null && o2==null)
+		{
+			toReturn=true;
+		}
+		
+		return toReturn;
+	}
+	
+	
+}
